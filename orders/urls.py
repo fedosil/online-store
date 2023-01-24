d4c48ef52aaeb295c40187722bb8bca50a8b6ed4
fedosil <fedosil@yandex.ru>
2023-01-24 16:31:30 +0300

    orders admin template

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from orders.views import Order_Create_View
+from orders.views import Order_Create_View, Success_View, Cancel_View
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create', Order_Create_View.as_view(), name='create'),
+    path('success', Success_View.as_view(), name='success'),
+    path('cancel', Cancel_View.as_view(), name='cancel'),
 ]
 
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,24 @@
+import stripe
+from http import HTTPStatus
+
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from orders.forms import Order_Form
 from common.views import Title_Mixin
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class Success_View(Title_Mixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Store - Спасибо за заказ!'
+
+class Cancel_View(TemplateView):
+    template_name = 'orders/cancel.html'
 
 class Order_Create_View(Title_Mixin, CreateView):
     template_name = 'orders/order-create.html'
@@ -11,6 +26,33 @@ class Order_Create_View(Title_Mixin, CreateView):
     success_url = reverse_lazy('orders:create')
     title = 'Store - Оформление заказа'
 
+    def post(self, request, *args, **kwargs):
+        super(Order_Create_View, self).post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1MSGgdKJN9Z2V10jGV18oo1Y',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:success')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:cancel')),
+        )
+        return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
+
     def form_valid(self, form):
         form.instance.initiator = self.request.user
         return super(Order_Create_View, self).form_valid(form)
+
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+
+    # For now, you only need to print out the webhook payload so you can see
+    # the structure.
+    print(payload)
+
+    return HttpResponse(status=200)
